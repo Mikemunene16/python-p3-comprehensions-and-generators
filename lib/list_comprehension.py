81e576b140a605174ca61c8f21ea3615a891d4c2
Mike <mike.munene@student.moringaschool.com>
2023-10-01 23:41:29 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+
+    even_elements = [x for x in num_list if x % 2 == 0]
+    
+    # Check if even_elements list is empty
+    if not even_elements:
+        return []  # Return an empty list if there are no even elements
+    else:
+        return even_elements  # Return the list of even elements
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_sentence_list = [sentence + '!' for sentence in sentence_list]
+    return exclaimed_sentence_list
